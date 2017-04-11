06b5add5834c863abce8dee2c6297fba8fe09266
m0sa <samo.prelog@gmail.com>
2017-04-11 19:03:28 +0200

    initial commit

---
+++ NuGet.FrameworkAssemblyPacker/AddFrameworkAssemblies.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Framework;
+
+namespace NuGet.FrameworkAssemblyPacker
+{
+    public class AddFrameworkAssemblies : Microsoft.Build.Utilities.Task
+    {
+        [Required]
+        public string NuSpecFile { get; set; }
+
+        [Required]
+        public string PatchedNuSpecFile { get; set; }
+
+        [Required]
+        public ITaskItem[] FrameworkAssemblies { get; set; }
+
+        public override bool Execute()
+        {
+            var content = File.ReadAllText(NuSpecFile);
+            if (content.IndexOf("<frameworkAssemblies>") < 0)
+            {
+                Log.LogError("'{0}' already contains a frameworkAssemblies element, you can probably don't need the NuGet.FrameworkAssemblyPacker package");
+                return false;
+            }
+
+            var end = content.IndexOf("</metadata>");
+            var newContent = content.Substring(0, end);
+            newContent += "<frameworkAssemblies>\n";
+            foreach (var r in FrameworkAssemblies ?? new ITaskItem[0])
+            {
+                var targetFramework = r.GetMetadata("TargetFramework");
+                if (string.IsNullOrEmpty(targetFramework))
+                {
+                    continue;
+                }
+                newContent += "    <frameworkAssembly assemblyName=\"" + r.ItemSpec + "\" targetFramework=\"" + targetFramework + "\" />\n";
+            }
+            newContent += "  </frameworkAssemblies>\n  ";
+            newContent += content.Substring(end);
+            File.WriteAllText(PatchedNuSpecFile, newContent);
+            return true;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # NuGet.FrameworkAssemblyPacker
+
+[A pollyfill for nuget Issue #](https://github.com/NuGet/Home/issues/4853) that allows adding `<frameworkAssemblies>` to nuget packages built with the `<Project Sdk="Microsoft.NET.Sdk">`.
+
+### Usage
+
+```
+Install-Package NuGet.FrameworkAssemblyPacker
+```
+
+In your `.csproj` add the `PackAsFrameworkAssembly` metadata to the `<Reference>` items that you want to end up in the package, e.g.
+
+```xml
+  <ItemGroup Condition=" '$(TargetFramework)' == 'net40' OR '$(TargetFramework)' == 'net45' ">
+    <Reference Include="System" />
+    <Reference Include="System.Data">
+      <PackAsFrameworkAssembly>true</PackAsFrameworkAssembly>
+    </Reference>
+    <Reference Include="System.Xml" PackAsFrameworkAssembly="true" />
+  </ItemGroup>
+```
+
+This would add the following fragment to the `.nuspec` file:
+
+```xml
+  <metadata>
+    <!-- ... -->
+    <frameworkAssemblies>
+      <frameworkAssembly assemblyName="System.Data" targetFramework="net40" />
+      <frameworkAssembly assemblyName="System.Xml" targetFramework="net40" />
+      <frameworkAssembly assemblyName="System.Data" targetFramework="net45" />
+      <frameworkAssembly assemblyName="System.Xml" targetFramework="net45" />
+    </frameworkAssemblies>
+  </metadata>
+```
